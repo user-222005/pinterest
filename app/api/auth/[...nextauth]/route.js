b00d376250bc8092a28e75db32a75571ca854dd9
dheena <dheena@mericaninc.com>
2023-07-10 16:21:25 +0530

    change nextconfig.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,33 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
-    // ...add more providers here
-  ],
-}
-export default NextAuth(authOptions)
+import GithubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
+// export const authOptions = {
+//   // Configure one or more authentication providers
+//   providers: [
+//     GoogleProvider({
+//         clientId: process.env.GOOGLE_CLIENT_ID,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//       })
+//     // ...add more providers here
+//   ],
+// }
+// export default NextAuth(authOptions)
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+          }),
+          FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+          })
+    ]
+})
+
+export {handler as GET, handler as POST};
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,14 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
 import { HiSearch,HiBell,HiChat } from "react-icons/hi";
-import { useSession, signIn } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react"
 const Header = () => {
     const {data: session} = useSession()
     console.log("Session", session);
 
   return (
-    
-    <div className='flex gap-3 md:gap-2 items-center p-4'>
+    <div className="flex gap-3 md:gap-2 items-center p-4">
       <Image
         src="/images/pinterest.png"
         alt="pinterest img"
@@ -16,19 +16,44 @@ const Header = () => {
         height={50}
         className="hover:bg-gray-300 p-2 rounded-full cursor-pointer"
       />
-      <button className='bg-black text-white p-2 rounded-full px-4'>Home</button>
-      <button className='font-semibold p-2 rounded-full px-4'>Explore</button>
-      <div className='bg-[#e9e9e9] p-3 gap-3 items-center rounded-full w-full hidden md:flex'>
-        <HiSearch className='text-[25px] text-gray-500 md:hidden'/>
-        <input type="text" placeholder="search" className='bg-transparent outline-none ' />
-        
-      </div>
-        <HiBell className='text-[25px] md:text-[40px] text-gray-500' />
-        <HiChat className='text-[25px] md:text-[40px] text-gray-500' />
-        <button className='bg-blue-500 text-white px-4' onClick={()=>signIn()}>Sign In</button>
-        <Image src="/images/profile.jpeg" alt="man-img" width={50} height={50} 
-        className="hover:bg-gray-300 p-2 rounded-full cursor-pointer"
+      <button className="bg-black text-white p-2 rounded-full px-4">
+        Home
+      </button>
+      <button className="font-semibold p-2 rounded-full px-4">Explore</button>
+      <div className="bg-[#e9e9e9] p-3 gap-3 items-center rounded-full w-full hidden md:flex">
+        <HiSearch className="text-[25px] text-gray-500 md:hidden" />
+        <input
+          type="text"
+          placeholder="search"
+          className="bg-transparent outline-none "
         />
+      </div>
+      <HiBell className="text-[20px] md:text-[40px] text-gray-500" />
+      <HiChat className="text-[20px] md:text-[40px] text-gray-500" />
+      {session==null?
+      <button
+        className="bg-blue-500 text-white rounded-lg px-4 p-2"
+        onClick={() => signIn()}
+      >
+        SignIn
+      </button>
+      :
+      <>
+      <button
+        className="bg-green-500 text-white rounded-lg px-4 p-2"
+        onClick={() => signOut()}
+      >
+        SignOut
+      </button>
+      <Image
+        src={session?.user.image}
+        alt="man-img"
+        width={50}
+        height={50}
+        className="hover:bg-gray-300 p-2 rounded-full cursor-pointer"
+      />
+      </>
+      }
     </div>
   );
 }
