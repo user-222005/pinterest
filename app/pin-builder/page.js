25335ce1b11107f834feea129dce18c53b5c5d22
dheena <dheena@mericaninc.com>
2023-07-12 12:04:10 +0530

    add :

--- app/pin-builder/page.js
+++ app/pin-builder/page.js
@@ -1,10 +1,10 @@
-import Form from "../../components/Form.jsx"
+import Form from "@/components/Form"
 
 const PinBuilder = () => {
   return (
     <div className='bg-[#e9e9e9] min-h-screen p-8 
     px-[10px] md:px-[160px]'>
-      <Form />
+      <Form />;
     </div>
   )
 }
