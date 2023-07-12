3c2c68211dbed8fe04159372a6a6599d7b9664cd
dheena <dheena@mericaninc.com>
2023-07-12 10:49:24 +0530

    create home page

--- components/form.jsx
+++ components/form.jsx
@@ -60,49 +60,62 @@ const Form=()=> {
    
    
   return (
-    <div className=' bg-white p-16 rounded-2xl '>
-        
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-10'>
-           
-            <UploadImage setFile={(file)=>setFile(file)} />
-          
-       <div className="col-span-2">
-       <div className='w-[100%]'>
-        <input type="text" placeholder='Add your Title'
-            onChange={(e)=>setTitle(e.target.value)}
-        className='text-[35px] outline-none font-bold w-full
-        border-b-[2px] border-gray-400 placeholder-gray-400'/>
-        <h2 className='text-[12px] mb-8 w-full  text-gray-400'>The first 40 Charaters are 
-        what usually show up in feeds</h2>
-        <UserTag user={session?.user} />
-        <textarea type="text"
-          onChange={(e)=>setDesc(e.target.value)}
-            placeholder='Tell everyone what your pin is about' 
-        className=' outline-none  w-full mt-8 pb-4 text-[14px]
-        border-b-[2px] border-gray-400 placeholder-gray-400'/>
-          <input type="text"
-          onChange={(e)=>setLink(e.target.value)}
-           placeholder='Add a Destination Link' 
-        className=' outline-none  w-full  pb-4 mt-[90px]
-        border-b-[2px] border-gray-400 placeholder-gray-400'/>
-    </div>
-    <div className='flex justify-end mb-6 p-4'>
-            <button onClick={()=>onSave()}
-             className='bg-red-500 p-2
+    <div className=" bg-white p-16 rounded-2xl ">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+        <UploadImage setFile={(file) => setFile(file)} />
+
+        <div className="col-span-2">
+          <div className="w-[100%]">
+            <input
+              type="text"
+              placeholder="Add your Title"
+              onChange={(e) => setTitle(e.target.value)}
+              className="text-[35px] outline-none font-bold w-full
+        border-b-[2px] border-gray-400 placeholder-gray-400"
+            />
+            <h2 className="text-[12px] mb-8 w-full  text-gray-400">
+              The first 40 Charaters are what usually show up in feeds
+            </h2>
+            <UserTag user={session?.user} />
+            <textarea
+              type="text"
+              onChange={(e) => setDesc(e.target.value)}
+              placeholder="Tell everyone what your pin is about"
+              className=" outline-none  w-full mt-8 pb-4 text-[14px]
+        border-b-[2px] border-gray-400 placeholder-gray-400"
+            />
+            <input
+              type="text"
+              onChange={(e) => setLink(e.target.value)}
+              placeholder="Add a Destination Link"
+              className=" outline-none  w-full  pb-4 mt-[90px]
+        border-b-[2px] border-gray-400 placeholder-gray-400"
+            />
+          </div>
+          <div className="flex justify-end mb-6 p-4">
+            <button
+              onClick={() => onSave()}
+              className="bg-red-500 p-2
             text-white font-semibold px-3 
-            rounded-lg'>
-              {loading?  <Image src="/images/loading.png" 
-                width={30} 
-                height={30} 
-                alt='loading'
-                className='animate-spin'  />:
-                <span>Save</span>}</button>
+            rounded-lg"
+            >
+              {loading ? (
+                <Image
+                  src="/images/loading.png"
+                  width={30}
+                  height={30}
+                  alt="loading"
+                  className="animate-spin"
+                />
+              ) : (
+                <span>Save</span>
+              )}
+            </button>
+          </div>
         </div>
-       </div>
-        
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Form;
