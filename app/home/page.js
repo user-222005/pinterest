0047552d5644fea939f46d36ab0cd17d89a31be2
dheena <dheena@mericaninc.com>
2023-07-12 11:34:24 +0530

    css added

--- app/home/page.js
+++ app/home/page.js
@@ -25,7 +25,7 @@ const HomePage = () => {
       });
   }
     return(
-      <div className='grid grid-cols-4'>
+      <div className='mt-7 px-2 md:px-5 columns-2 md:columns-3 lg:columns-4 xl:columns-5 space-y-6 mx-auto'>
         {post.map((item,index)=>{
           return(
             <div key={index}>
--- components/pinList.jsx
+++ components/pinList.jsx
@@ -10,7 +10,7 @@ const PinList = ({pin}) => {
 
 
   return (
-    <div  className='grid grid-cols-4 mt-4'>
+    <div  className='mt-7 px-2 md:px-5 columns-2 md:columns-3 lg:columns-4 xl:columns-5 space-y-6 mx-auto'>
         {pin.map((item,id)=>{
             return(
                 <div key={id}>
