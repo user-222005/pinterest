30b4fc910a60c32fcbc5824c9828e450a8461d60
dheena <dheena@mericaninc.com>
2023-07-12 15:31:53 +0530

    added routing

--- app/firstpage/page.js
+++ app/firstpage/page.js
@@ -1,7 +1,11 @@
+'use client'
+import { signIn } from 'next-auth/react';
+// import { useRouter } from 'next/navigation';
 import React from 'react'
 import {GrLinkNext} from "react-icons/gr"
 
 const FirstPage = () => {
+    // const router = useRouter()
   return (
     <div className="w-md-screen h-screen flex justify-center items-center flex-col" style={{
         backgroundImage: `url("https://images.pexels.com/photos/1229861/pexels-photo-1229861.jpeg?auto=compress&cs=tinysrgb&w=600")`,
@@ -15,8 +19,9 @@ const FirstPage = () => {
       <button
         type="button"
         className="text-white bg-gradient-to-br mt-4 from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      onClick={()=>signIn()}
       >
-        Login <GrLinkNext  className=' text-white'/>
+        Login <GrLinkNext  className=''/>
       </button>
     </div>
   );
